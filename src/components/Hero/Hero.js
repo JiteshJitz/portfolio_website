0dd26f5e1b16ad7ff4242c62bffec3bccb374dfb
Jitesh Rajamanikyam Udayakumar <jrajaman@uwaterloo.ca>
2022-05-26 21:38:03 -0400

    Resume updated

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         <SectionText>
         Graduate student passionate about Software Engineering and AI domain. With more than four and half years of experience working with ML, software, and IOT related personal projects.
         </SectionText>
-        <Button  ><a  href="https://drive.google.com/uc?export=download&id=1LfOrMee1q5vYVCbbAR8VvROcGv0DR2kG" target="_blank">My Resume</a></Button>
+        <a  href="https://drive.google.com/file/d/1UVC6DVgstI7ZvHp23mk3I_onbFLPXBcI/view?usp=sharing" target="_blank"><Button  >My Resume</Button></a>
       </LeftSection>
     </Section>
   </>
