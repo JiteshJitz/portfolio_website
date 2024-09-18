b31969ff3fce2aedaa437f0502f272e5a00a5411
Jitesh Rajamanikyam Udayakumar <jiteshru@v1041-wn-rt-a-215-182.campus-dynamic.uwaterloo.ca>
2024-09-17 20:27:05 -0400

    added new resume link

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         <SectionText>
         Full-Stack Developer with expertise in MERN, Spring Boot, and mobile applications. Committed to delivering secure, innovative and scalable software solutions.
         </SectionText>
-        <a  href="https://drive.google.com/file/d/12QTfOyrf1gKFaE6V2HxWrs11u3IFeIY0/view?usp=drive_link" target="_blank"><Button  >My Resume</Button></a>
+        <a  href="https://drive.google.com/file/d/1b7iw0GBgd1EuTVdMR3WStqUm-rZpq4Qh/view?usp=sharing" target="_blank"><Button  >My Resume</Button></a>
       </LeftSection>
     </Section>
   </>
